ddc4a5c2e94fa76b7c051b82547056562040bc6f
mingking2 <mingee641@gmail.com>
2024-12-10 13:34:51 +0900

    init: 초기설정

---
+++ src/main/java/template/JavaTemplateApplication.java
@@ -0,0 +1,13 @@
+package template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTemplateApplication.class, args);
+    }
+
+}
